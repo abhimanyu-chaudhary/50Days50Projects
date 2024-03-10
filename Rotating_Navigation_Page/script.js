46f5e304ab0ad2c55e25cd1779d84973710b325a
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-03-10 16:59:51 +0530

    Rotating Navigation Page
    Some rectification pending.

---
+++ Rotating_Navigation_Page/script.js
@@ -0,0 +1,12 @@
+function turnup(){
+    document.querySelector(".nav").classList.add("nav-after");
+    document.querySelector(".container").classList.add("center-body-after");
+    document.querySelector(".slider").classList.add("slider-after");
+}
+
+
+function turndown(){
+    document.querySelector(".nav").classList.remove("nav-after");
+    document.querySelector(".container").classList.remove("center-body-after");
+    document.querySelector(".slider").classList.remove("slider-after");
+}
